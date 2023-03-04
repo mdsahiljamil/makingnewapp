7b45f8955b1fbf094ad5ad522980353596816e0c
mdsahiljamil <mdsahiljamil2000@gmail.com>
2023-03-05 00:19:39 +0530

    adding text

--- lib/screen/myhomepagr.dart
+++ lib/screen/myhomepagr.dart
@@ -28,6 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            const Text("hello world"),
             const Text(
               'You have pushed the button this many times:',
             ),
